dbbc56be7d561d1ac422998714f11d9a86c374f4
BadAimWeeb <badaimweeb@protonmail.com>
2022-07-29 18:47:46 +0700

    add types for v0r10

---
+++ README.md
@@ -0,0 +1,15 @@
+# NOCOM_BOT A1-Type Plugin support package types
+
+This repository add types to support package provided by A-Type handler.
+
+Install:
+
+```bash
+npm i --save-dev @nocom_bot/types_ts_plugin_a1
+```
+
+Usage: Add this line to where you use the support package:
+
+```ts
+import type "@nocom_bot/types_ts_plugin_a1";
+```
---
+++ index.d.ts
@@ -0,0 +1,61 @@
+declare module "@nocom_bot/nocom-atype-support" {
+    export interface ICommandData {
+        interfaceID: number,
+
+        cmd: string,
+        args: string[],
+        attachments: {
+            filename: string,
+            url: string
+        }[],
+        mentions: {
+            [formattedUserID: string]: {
+                start: number,
+                length: number
+            }
+        },
+        originalContent: string,
+        prefix: string,
+
+        messageID: string,
+        formattedMessageID: string,
+        channelID: string,
+        formattedChannelID: string,
+        senderID: string,
+        formattedSenderID: string,
+        guildID: string,
+        formattedGuildID: string,
+
+        language: string,
+        additionalInterfaceData?: any
+    }
+
+    export function verifyPlugin(allow: boolean): void;
+    export function callFuncPlugin(namespace: string, funcName: string, ...args: any): Promise<any>;
+    export function registerFuncPlugin(funcName: string, funcCallTarget: Function): Promise<boolean>;
+    export function callAPI(moduleID: string, cmd: string, data: any): Promise<any>;
+    export function registerCommand(commandName: string, commandDescriptionAPI: (language: string) => {
+        args: string,
+        desc: string
+    }, commandCallback: (data: ICommandData) => Promise<{
+        content: string,
+        attachments: {
+            filename?: string,
+            data: Buffer | string
+        }[],
+        additionalInterfaceData?: any
+    }>, compatibility: string[]): Promise<boolean>;
+
+    export function registerCommandFuncPlugin(commandName: string, funcDescAPITarget: string, funcName: string, compatibility: string[]): Promise<boolean>;
+    export function exit(exitCode?: number, exitReason?: string): void;
+    export function waitForModule(moduleNamespace: string, timeout: number): Promise<boolean>;
+
+    export const log = {
+        critical: (...data: any[]) => Promise<void>,
+        error: (...data: any[]) => Promise<void>,
+        warn: (...data: any[]) => Promise<void>,
+        info: (...data: any[]) => Promise<void>,
+        debug: (...data: any[]) => Promise<void>,
+        verbose: (...data: any[]) => Promise<void>
+    }
+}
